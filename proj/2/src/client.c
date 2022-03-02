221979e5c59e3db3258adb9785807b0adf14ba12
netos23 <nikmoroz2002@outlook.com>
2022-03-02 10:06:26 +0300

    add alt task2

--- proj/2/src/client.c
+++ proj/2/src/client.c
@@ -1,77 +1,28 @@
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <sys/file.h>
 
-#define FILE_POSITION 1
-#define TRUNC_SIZE 5000
+#define BLOCK 512
 
-bool isExecuted = true;
-
-void sigTermHandler(int sig) {
-    if (sig == SIGTERM) {
-        isExecuted = false;
-    }
-}
-
-void sigContHandler(int sig) {
-    // empty handler
-}
-
-int main(int argc, char **argv) {
-
-    if (argc < 2) {
-        puts("Usage: client <file> <message>");
-        return 0;
-    }
-
-    if (signal(SIGTERM, sigTermHandler) == SIG_ERR) {
-        puts("can`t bind SIGTERM handler");
-        return 1;
-    }
-
-    if (signal(SIGCONT, sigContHandler) == SIG_ERR) {
-        puts("can`t bind SIGCONT handler");
-        return 1;
-    }
-
-    int buffer_fd;
-    FILE *buffer_f;
-    if (!(buffer_f = fopen(argv[FILE_POSITION], "w+"))) {
-        puts("Can`t open file\n");
-        return 1;
-    }
-    buffer_fd = fileno(buffer_f);
-    pid_t pid = getpid();
-
-    printf("Client started with pid %d! Waiting for server... \n", pid);
-    pause();
-
-    while (isExecuted && !flock(buffer_fd, LOCK_EX)) {
-        printf("Client with pid %d writes:\n", pid);
-        ftruncate(buffer_fd, 0);
-
-        for (int i = 2; i < argc; i++) {
-            char *buf = argv[i];
-            int buflen = (int) strlen(buf);
-            write(buffer_fd, buf, buflen);
-            printf("%.*s ", buflen, buf);
+void main() {
+    int fd = open("data", O_RDWR);
+    char data[BLOCK];
+    while (1) {
+        if (flock(fd, LOCK_EX) != -1) {
+            int sz = read(fd, data, sizeof(data));
+            while (sz > 0) {
+                sz = read(fd, data, sizeof(data));
+            }
+            printf("input: ");
+            scanf("%s", data);
+            sz = write(fd, data, sizeof(data));
+            printf("Client writes : %s\n", data);
+            flock(fd, LOCK_UN);
+            sleep(1);
         }
-        putchar('\n');
-
-        fflush(buffer_f);
-        fflush(stdout);
-        rewind(buffer_f);
-
-        flock(buffer_fd, LOCK_UN);
-
-        pause();
     }
-
-    close(buffer_fd);
-    puts("Shut down client\n");
-
-    return 0;
+    close(fd);
 }
